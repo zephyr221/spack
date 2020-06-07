1e2cfff0cac3601a169de0e3e9dc74cdbbb401f1
Todd Gamblin <tgamblin@llnl.gov>
2020-06-07 11:09:18 -0700

    concretizer: add a configuration option to use new or old concretizer
    - [x] spec.py can call out to the new concretizer
    - [x] config.yaml now has an option to choose a concretizer (original, clingo)

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -103,6 +103,7 @@ config_defaults = {
         'dirty': False,
         'build_jobs': min(16, multiprocessing.cpu_count()),
         'build_stage': '$tempdir/spack-stage',
+        'concretizer': 'original',
     }
 }
 
